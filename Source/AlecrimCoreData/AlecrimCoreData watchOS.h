9b01989903f82012ac5f35a8544a4645200c75f6
Tobias Ottenweller <tottenweller@babbel.com>
2015-07-28 12:28:47 +0200

    seperate target for watchOS added.

---
+++ Source/AlecrimCoreData/AlecrimCoreData watchOS.h
@@ -0,0 +1,18 @@
+//
+//  AlecrimCoreData watchOS.h
+//  AlecrimCoreData
+//
+//  Created by Tobias Ottenweller on 28/07/15.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for AlecrimCoreData watchOS.
+FOUNDATION_EXPORT double AlecrimCoreData_watchOSVersionNumber;
+
+//! Project version string for AlecrimCoreData watchOS.
+FOUNDATION_EXPORT const unsigned char AlecrimCoreData_watchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlecrimCoreData_watchOS/PublicHeader.h>
+
+
